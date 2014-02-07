85233adca20f5f31c54f5aa261dd13e0afee5246
Melissa Carbone <mcarbone1@gmail.com>
2014-02-07 16:39:42 -0500

    Add moovy spec directory and movie spec

---
+++ spec/moovy/movie_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe Moovy::Movie do
+  it 'allows me to search for a movie title' do
+    movies = Moovy::Movie.search('Frozen')
+    expect(movies).to_not eql([])
+    expect(movies.first).to be_kind_of(Moovy::Movie)
+  end
+end
